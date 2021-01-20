2abe796e295a9b3ea6ac1adda8ed0bd0b68f94f2
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-01-20 16:49:50 +0300

    Updates on configuration

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,29 +13,29 @@ repositories {
 }
 
 dependencies {
-  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${Dependencies.Plugins.KOTLIN}")
-  implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:${Dependencies.Plugins.DETEKT}")
-  implementation("com.github.breadmoirai:github-release:${Dependencies.Plugins.GITHUB_RELEASE}")
-  implementation("com.github.jengelman.gradle.plugins:shadow:${Dependencies.Plugins.SHADOW}")
-  implementation("com.github.ben-manes:gradle-versions-plugin:${Dependencies.Plugins.VERSIONS}")
-  implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:${Dependencies.Plugins.SONAR}")
-  implementation("org.jetbrains.dokka:dokka-gradle-plugin:${Dependencies.Plugins.DOKKA}")
-  implementation("com.vdurmont:semver4j:${Dependencies.Plugins.SEMVER4J}")
-  implementation("io.codearte.gradle.nexus:gradle-nexus-staging-plugin:${Dependencies.Plugins.NEXUS}")
-  implementation("org.jetbrains.kotlinx:binary-compatibility-validator:${Dependencies.Plugins.KOTLIN_API_VALIDATOR}")
+  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${Dependencies.Plugins.KOTLIN_VERSION}")
+  implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:${Dependencies.Plugins.DETEKT_VERSION}")
+  implementation("com.github.breadmoirai:github-release:${Dependencies.Plugins.GITHUB_RELEASE_VERSION}")
+  implementation("com.github.jengelman.gradle.plugins:shadow:${Dependencies.Plugins.SHADOW_VERSION}")
+  implementation("com.github.ben-manes:gradle-versions-plugin:${Dependencies.Plugins.VERSIONS_VERSION}")
+  implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:${Dependencies.Plugins.SONAR_VERSION}")
+  implementation("org.jetbrains.dokka:dokka-gradle-plugin:${Dependencies.Plugins.DOKKA_VERSION}")
+  implementation("com.vdurmont:semver4j:${Dependencies.Plugins.SEMVER4J_VERSION}")
+  implementation("io.codearte.gradle.nexus:gradle-nexus-staging-plugin:${Dependencies.Plugins.NEXUS_VERSION}")
+  implementation("org.jetbrains.kotlinx:binary-compatibility-validator:${Dependencies.Plugins.KOTLIN_API_VALIDATOR_VERSION}")
 }
 
 object Dependencies {
   object Plugins {
-    const val KOTLIN = "1.4.21"
-    const val DETEKT = "1.15.0"
-    const val GITHUB_RELEASE = "2.2.12"
-    const val SHADOW = "5.2.0"
-    const val VERSIONS = "0.28.0"
-    const val SONAR = "2.8"
-    const val DOKKA = "1.4.10"
-    const val SEMVER4J = "3.1.0"
-    const val NEXUS = "0.22.0"
-    const val KOTLIN_API_VALIDATOR = "0.3.0"
+    const val KOTLIN_VERSION = "1.4.21"
+    const val DETEKT_VERSION = "1.15.0"
+    const val GITHUB_RELEASE_VERSION = "2.2.12"
+    const val SHADOW_VERSION = "5.2.0"
+    const val VERSIONS_VERSION = "0.28.0"
+    const val SONAR_VERSION = "2.8"
+    const val DOKKA_VERSION = "1.4.10"
+    const val SEMVER4J_VERSION = "3.1.0"
+    const val NEXUS_VERSION = "0.22.0"
+    const val KOTLIN_API_VALIDATOR_VERSION = "0.3.0"
   }
 }
