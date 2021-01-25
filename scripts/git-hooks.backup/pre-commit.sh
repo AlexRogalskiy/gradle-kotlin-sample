93790cac2846daf7d9c56d081f85e800a1c2edea
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-01-25 16:53:29 +0300

    Updates on configuration

--- scripts/git-hooks.backup/pre-commit.sh
+++ scripts/git-hooks.backup/pre-commit.sh
@@ -5,7 +5,7 @@ JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 export JAVA_HOME
 
 OUTPUT="/tmp/analysis-result"
-./gradlew lintDebug detekt ktlintCheck spotlessApply --daemon >${OUTPUT}
+./gradlew detekt ktlintCheck spotlessApply --daemon >${OUTPUT}
 EXIT_CODE=$?
 if [ ${EXIT_CODE} -ne 0 ]; then
   cat ${OUTPUT}
